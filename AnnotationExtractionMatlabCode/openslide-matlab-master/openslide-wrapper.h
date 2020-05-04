222202250d6a1e61bf5f6d36c16392026a86e7a8
AzmHmd <azamhamidinekoo@20345MOPLT>
2020-05-04 11:16:10 +0100

    added AnnotationExtractionMatlabCode

--- AnnotationExtractionMatlabCode/openslide-matlab-master/openslide-wrapper.h
+++
@@ -1,5 +0,0 @@
-#ifndef MATLAB_OPENSLIDE_WRAPPER_H
-#define MATLAB_OPENSLIDE_WRAPPER_H
-#define OPENSLIDE_SIMPLIFY_HEADERS
-#include "C:\tools\openslide-win64-20171122\include\openslide\openslide.h"
-#endif
--- AnnotationExtractionMatlabCode/openslide-matlab-master/test_data/README.txt
+++
@@ -1,3 +0,0 @@
-This test data has been downloaded from http://openslide.cs.cmu.edu/download/openslide-testdata/.
-
-More test data from various vendors can be found on this site.
